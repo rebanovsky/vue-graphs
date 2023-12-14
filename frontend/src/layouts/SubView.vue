ca5e295ad03f27cdb26b9b1a760ca13dd8e54965
rebanovsky <58785104+rebanovsky@users.noreply.github.com>
2023-12-14 23:08:46 +0200

    ui fixes

--- frontend/src/layouts/SubView.vue
+++ frontend/src/layouts/SubView.vue
@@ -1,13 +1,13 @@
 <template>
   <div
     class="playground-component w-[100%] flex justify-between px-[12px] rounded-[14px]"
-    style="height: calc(100vh - 90px)"
+    style="height: calc(100vh - 73px)"
   >
     <div class="main-content w-full overflow-y-auto flex justify-center">
       <div class="sidenav overflow-y-auto gridlines rounded-[12px]" style="height: calc(100vh - 90px)">
         <SideMenu :title="title" :items="sidenavItems" :base-path="basePath" />
       </div>
-      <div class="flex w-[100%] flex-1 justify-center">
+      <div class="flex w-[100%] flex-1 justify-center overflow-y-auto">
         <router-view v-slot="{ Component }">
           <!-- <transition name="fade" mode="out-in"> -->
             <component :is="Component" />
